966d3e61e7cc1ec3cdc8c2841958a555a77e3af1
Jane <evgeniiatsyp13@gmail.com>
2023-03-07 20:56:53 +0100

    example of default props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
-const Header = ({title}) => {
+const Header = (props) => {
   return (
     <div className="container">
     <header>
-        <h1>{title}</h1>
+        <h1>{props.title}</h1>
     </header>
     </div>
   )
+  
+}
+Header.defaultProps = {
+    title : "Task Tracker"
 }
 
 export default Header
