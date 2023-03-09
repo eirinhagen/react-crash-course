7aa7d05c2a97f924b2202d1aa28b1302536ad909
Jane <evgeniiatsyp13@gmail.com>
2023-03-09 07:28:55 +0100

    example of usesState

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,14 +1,15 @@
-const tasks = [
-    {id : 1,
-     text : "Coocking salmon",
-     reminder : true
-    },
-    {id : 2,
-        text : "Eating salmon",
-        reminder : false
-       }
-]
+import { useState } from "react"
 const Tasks = () => {
+    const [tasks,setTasks] = useState([
+        {id : 1,
+         text : "Coocking salmon",
+         reminder : true
+        },
+        {id : 2,
+            text : "Eating salmon",
+            reminder : false
+           }
+    ])
   return (
     <>
     {tasks.map((task)=>(<h3 key={task.id}>{task.text}</h3>))}
